1c73a3bb11b508a8863b00f7ec9f91fcd2423916
Sridhar <ronsridhar607@gmail.com>
2024-06-18 18:00:35 +0530

    Resume Uploaded

--- src/Components/About.js
+++ src/Components/About.js
@@ -1,19 +1,41 @@
 const About = () => {
   return (
     <>
-<div className="aboutsection">
-            <div className="container">
-               <p>8+ years of experience in IT industry as a Front-End UI Developer with a solid understanding of Application designing & development and installation of different modules.</p>
-<p>Experience in working on projects in highly collaborative, multi-disciplinary development teams.</p>
-<p>Excellent ability to write clear, efficient, optimized, and reusable code for web development.</p>
-<p>Professional in Web page Audit using Browser Developer Tools, Cross Browser Compatibility & Web Accessibility. </p>
-<p>Business Areas: Web & Mobile Application, Banking, E-Commerce,  Financial, ERP, Creative, Digital Marketing</p>
-<p>Advanced knowledge of React, JavaScript & AJAX, HTML5, CSS & Bootstrap    </p>
-<p>Able to work independently and manage high-profile projects and experience in complete SDLC cycle.</p>
-            </div>
+      <div className="aboutsection">
+        <div className="container">
+          <p>
+            10+ years of experience in IT industry as a Front-End UI Developer
+            with a solid understanding of Application designing & development
+            and installation of different modules.
+          </p>
+          <p>
+            Experience in working on projects in highly collaborative,
+            multi-disciplinary development teams.
+          </p>
+          <p>
+            Excellent ability to write clear, efficient, optimized, and reusable
+            code for web development.
+          </p>
+          <p>
+            Professional in Web page Audit using Browser Developer Tools, Cross
+            Browser Compatibility & Web Accessibility.{" "}
+          </p>
+          <p>
+            Business Areas: Web & Mobile Application, Banking, E-Commerce,
+            Financial, ERP, Creative, Digital Marketing
+          </p>
+          <p>
+            Advanced knowledge of React, JavaScript & AJAX, HTML5, CSS &
+            Bootstrap{" "}
+          </p>
+          <p>
+            Able to work independently and manage high-profile projects and
+            experience in complete SDLC cycle.
+          </p>
         </div>
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default About
+export default About;
--- src/Components/Profileinfo.js
+++ src/Components/Profileinfo.js
@@ -1,15 +1,28 @@
 const Profileinfo = () => {
   return (
-    <div  id="about">
-        <h1 className="pghead">SRIDHAR S</h1>
-        <ul className="infouser">
-            <li> <img src="./location.svg"/> Chennai, India</li>
-            <li><img src="./phone.svg"/> <a href="tel:+919597280532">+91 9597280532</a></li>
-            <li><img src="./envelope.svg"/> <a href="mailto:ronsridhar607@gmail.com">ronsridhar607@gmail.com</a></li>
-            <li><a href="mailto:ronsridhar607@gmail.com">Download CV</a></li>
-        </ul>
+    <div id="about">
+      <h1 className="pghead">SRIDHAR S</h1>
+      <ul className="infouser">
+        <li>
+          {" "}
+          <img src="./location.svg" /> Chennai, India
+        </li>
+        <li>
+          <img src="./phone.svg" />{" "}
+          <a href="tel:+919597280532">+91 9597280532</a>
+        </li>
+        <li>
+          <img src="./envelope.svg" />{" "}
+          <a href="mailto:ronsridhar607@gmail.com">ronsridhar607@gmail.com</a>
+        </li>
+        <li>
+          <a href="./sridhar-resume.pdf" target="_blank" download="sridhar.pdf">
+            Download CV
+          </a>
+        </li>
+      </ul>
     </div>
-  )
-}
+  );
+};
 
-export default Profileinfo
+export default Profileinfo;
